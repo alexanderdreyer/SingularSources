c21475359c5dc1dae1c4ac3ca39861fb088ad34e
Alexander Dreyer <alexander.dreyer@itwm.fraunhofer.de>
2012-06-30 23:59:24 +0200

    new: n-ary operations for reference

--- Singular/countedref.cc
+++ Singular/countedref.cc
@@ -35,39 +35,81 @@
  * It also stores the Singular token number, once per type.
  **/
 class CountedRefData {
+ typedef CountedRefData self;
 public:
   typedef int id_type;
   typedef unsigned long count_type;
 
   /// Construct reference for Singular object
-  CountedRefData(leftv data): m_count(0) {
-    memset(&m_data, 0, sizeof(sleftv));
-    m_data.Copy(data);
-  }
+  CountedRefData(leftv data): m_count(0) { init(data); }
 
   /// Deep Copy
   CountedRefData(const CountedRefData& to_copy): m_count(0) {
-    memset(&m_data, 0, sizeof(sleftv));
-    m_data.Copy(const_cast<leftv>(&to_copy.m_data));
+    init(to_copy);
   }
 
-  ~CountedRefData()  { }
+  /// Deep copy of n-ary argument sequence
+  CountedRefData(const CountedRefData& to_copy, leftv next): m_count(0) {
+    init(to_copy);
+    leftv tmp = &m_data;
+    while (next) {
+      (tmp->next)=(leftv)omAlloc0(sizeof(sleftv));
+      (tmp->next)->Copy(is_ref(next)? self(next).get(): next);
+      tmp = tmp->next;
+      next = next->next;
+    }
+  }
 
-  const leftv operator*() {
+  /// Destructor
+  ~CountedRefData()  { m_data.CleanUp(); }
+
+  /// Assignment
+  self& operator=(const self& rhs) {
+    m_data.CleanUp();
+    init(rhs);
+    return *this;
+  }
+
+  /// Access ti actual object
+  leftv get() {
     return &m_data;
   }
-  operator char*() {
-    sleftv result;
-    memset(&result, 0, sizeof(sleftv));
-    result.Copy(&m_data);
-    return omStrDup(result.String());
+
+  /// @name Forward operations
+  //@{
+  BOOLEAN operator()(int op, leftv result) {
+    return iiExprArith1(result, get(), op);
   }
+  BOOLEAN operator()(int op, leftv result, leftv arg) {
+    return iiExprArith2(result, get(), op,
+                       (is_ref(arg)? cast(arg).get(): arg));
+  }
+  BOOLEAN operator()(int op, leftv result, leftv arg1, leftv arg2) {
+    return iiExprArith3(result, op, get(),
+                        (is_ref(arg1)? cast(arg1).get(): arg1),
+                        (is_ref(arg2)? cast(arg2).get(): arg2));
+  }
+  //@}
 
   /// Set Singular type identitfier 
   static void set_id(id_type new_id) {  access_id() = new_id;  }
 
   /// Get Singular type identitfier 
   static id_type id() { return access_id(); }
+
+  static self cast(leftv value) {
+    assume((value != NULL) && is_ref(value));
+    return self(*static_cast<CountedRefData*>(value->Data()),value->next);
+  }
+
+  /// Extract and deeply copy reference from Singular interpreter object data
+  static self cast(void* data) {
+    assume(data != NULL);
+    return self(*static_cast<CountedRefData*>(data));
+  }
+
+  /// Check for being reference in Singular interpreter object
+  static BOOLEAN is_ref(leftv arg) { return (arg->Typ() == id()); }
 
   /// @name Reference counter management
   //@{
@@ -77,39 +119,40 @@ public:
   //@}
 
 private:
+  /// Gain our own copy of argument
+  void init(leftv arg) {
+    memset(&m_data, 0, sizeof(sleftv));
+    m_data.Copy(arg);
+  }
+
+  /// Gain deep copy
+  void init(const self& rhs) {
+    init(const_cast<leftv>(&rhs.m_data));
+  }
+
+  /// Access identifier (one per class)
   static id_type& access_id() {
     static id_type g_id = 0;
     return g_id;
   }
-
+  /// Reference counter
   count_type m_count;
+
+  /// Singular object
   sleftv m_data;
 };
 
 /// blackbox support - initialization
 void* countedref_Init(blackbox*)
 {
-
     return NULL;
-   // CountedRefData* result = new CountedRefData(NULL);
-  //  result->operator++();
-  //  return result;
 }
 
 /// blackbox support - convert to string representation
 char* countedref_String(blackbox *b, void* ptr)
 {
   CountedRefData tmp(*static_cast<CountedRefData*>(ptr));
-  return (*tmp)->String();
-
-  Print("string called");
-
- // if (ptr == NULL)  return omStrDup("oo");
-
-//    sleftv result;
-//    memset(&result, 0, sizeof(sleftv));
-//    result.Copy((leftv)ptr);
-    return omStrDup("undef");//result.String());
+  return (tmp.get())->String();
 }
 
 /// blackbox support - copy element
@@ -124,7 +167,6 @@ BOOLEAN countedref_Assign(leftv l, leftv r)
 {
   CountedRefData* result = (r->Typ() == CountedRefData::id()? 
                             (CountedRefData*)r->Data(): new CountedRefData(r));
-
   if(result)
     result->operator++();
 
@@ -132,9 +174,7 @@ BOOLEAN countedref_Assign(leftv l, leftv r)
     IDDATA((idhdl)l->data) = (char *)result;
   else
     l->data = (void *)result;
-  
 
-  //  Print( "ass %i, %i, %p  ,%i \n", l->Typ(), r->Typ(), result->operator*(), result->operator*()->Typ() );
   return !result;
 }
                                                                      
@@ -142,7 +182,6 @@ BOOLEAN countedref_Assign(leftv l, leftv r)
 /// blackbox support - unary operations
 BOOLEAN countedref_Op1(int op, leftv res, leftv head)
 {
-  Print("op type %i %i\n", op, head->Typ());
   switch(op)
   {
   case TYPEOF_CMD:
@@ -151,56 +190,33 @@ BOOLEAN countedref_Op1(int op, leftv res, leftv head)
     return FALSE;
   }
 
-  CountedRefData tmp(*static_cast<CountedRefData*>(head->Data()));
-  return iiExprArith1(res, *tmp, op);
+  return CountedRefData::cast(head)(op, res);
 }
 
 
 /// blackbox support - binary operations
-BOOLEAN countedref_Op2(int op, leftv res, leftv arg1, leftv arg2)
+BOOLEAN countedref_Op2(int op, leftv res, leftv head, leftv arg)
 {
-  CountedRefData copy1(*static_cast<CountedRefData*>(arg1->Data()));
-
-  if (arg2->Typ() != CountedRefData::id())  
-     return iiExprArith2(res, *copy1, op, arg2);
-
-  CountedRefData copy2(*static_cast<CountedRefData*>(arg2->Data()));
-  return iiExprArith2(res, *copy1, op, *copy2, op);
+  return CountedRefData::cast(head)(op, res, arg);
 }
 
 /// blackbox support - ternary operations
-BOOLEAN countedref_Op3(int op, leftv res, leftv arg1, leftv arg2, leftv arg3)
+BOOLEAN countedref_Op3(int op, leftv res, leftv head, leftv arg1, leftv arg2)
 {
-  CountedRefData copy1(*static_cast<CountedRefData*>(arg1->Data()));
-  if (arg2->Typ() == CountedRefData::id()) {
-    CountedRefData copy2(*static_cast<CountedRefData*>(arg2->Data()));
-    if (arg3->Typ() == CountedRefData::id()) {
-       CountedRefData copy3(*static_cast<CountedRefData*>(arg3->Data()));
-       return iiExprArith3(res, op, *copy1, *copy2, *copy3);
-    }
-    return iiExprArith3(res, op, *copy1, *copy2, arg3);
-  }
-  if (arg3->Typ() == CountedRefData::id()) {
-     CountedRefData copy3(*static_cast<CountedRefData*>(arg3->Data()));
-     return iiExprArith3(res, op, *copy1, arg2, *copy3);
-  }
-  return iiExprArith3(res, op, *copy1, arg2, arg3);
+   return CountedRefData::cast(head)(op, res, arg1, arg2);
 }
 
 
 /// blackbox support - n-ary operations
 BOOLEAN countedref_OpM(int op, leftv res, leftv args)
 {
-  CountedRefData copy(*static_cast<CountedRefData*>(args->Data()));
-  return iiExprArithM(res, *copy, op);
+  return iiExprArithM(res, CountedRefData::cast(args).get(), op);
 }
 
 /// blackbox support - destruction
 void countedref_destroy(blackbox *b, void* ptr)
 {
   CountedRefData* pRef = static_cast<CountedRefData*>(ptr);
-
-  Print("kill %i\n", pRef->count());
   if(!pRef->operator--())
     delete pRef;
 }
